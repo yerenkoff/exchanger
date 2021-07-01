4bc083e7ee9088474019141bac021c9559179a7e
Kirill Yerenkoff <32268957+yerenkoff@users.noreply.github.com>
2021-07-01 18:12:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 let menuButtons = document.getElementsByClassName("menuButton");
 
-
-for (let menuButton of menuButtons) {
-    // console.log(menuButton);
-    let navHeight = menuButton.nextElementSibling.offsetHeight;
-    menuButton.nextElementSibling.style.maxHeight = 0;
-    console.log(navHeight);
-    menuButton.onclick = () => {
-        // e.target.classList.toggle("accordeon");
-        menuButton.nextElementSibling.classList.toggle("accordeon");
-        if (menuButton.nextElementSibling.offsetHeight == 0) {
-            menuButton.style.color = "#F0B90B";
-            menuButton.children[0].style.transform = "rotate(0deg)";
-            menuButton.children[0].style.filter = "grayscale(0%)";
-            menuButton.nextElementSibling.style.maxHeight = navHeight + "px";
-        } else {
-            menuButton.style.color = "#5d5d5d";
-            menuButton.children[0].style.transform = "rotate(-180deg)";
-            menuButton.children[0].style.filter = "grayscale(100%)";
-            menuButton.nextElementSibling.style.maxHeight = 0 + "px";
+if (window.matchMedia("(max-width: 700px)").matches) {
+    for (let menuButton of menuButtons) {
+        // console.log(menuButton);
+        let navHeight = menuButton.nextElementSibling.offsetHeight;
+        menuButton.nextElementSibling.style.maxHeight = 0;
+        console.log(navHeight);
+        menuButton.onclick = () => {
+            // e.target.classList.toggle("accordeon");
+            menuButton.nextElementSibling.classList.toggle("accordeon");
+            if (menuButton.nextElementSibling.offsetHeight == 0) {
+                menuButton.style.color = "#F0B90B";
+                menuButton.children[0].style.transform = "rotate(0deg)";
+                menuButton.children[0].style.filter = "grayscale(0%)";
+                menuButton.nextElementSibling.style.maxHeight = navHeight + "px";
+            } else {
+                menuButton.style.color = "#5d5d5d";
+                menuButton.children[0].style.transform = "rotate(-180deg)";
+                menuButton.children[0].style.filter = "grayscale(100%)";
+                menuButton.nextElementSibling.style.maxHeight = 0 + "px";
+            }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
